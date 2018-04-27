114b7b6f2bff542da04e85dbd8c2fc4c75a13235
Bradley <bradleysteele2000@gmail.com>
2018-04-27 09:35:43 +0100

    Added tests for was_published_recently() bug fix

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,33 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        future_question = Question(pub_date=timezone.now() + datetime.timedelta(days=30))
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        old_question = Question(pub_date=timezone.now() - datetime.timedelta(days=1, seconds=1))
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        recent_question = Question(pub_date=timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59))
+        self.assertIs(recent_question.was_published_recently(), True)
