068cac00ceb0dcfa495e4b598d96b81b0ac40a9b
Bradley Steele <bradleysteele2000@gmail.com>
2018-05-02 15:27:59 +0100

    Added page model

--- mysite/wiki/models.py
+++ mysite/wiki/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Page(models.Model):
+    page_title = models.CharField(max_length=100)
+    page_contents = models.CharField(max_length=3000)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.page_title
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
