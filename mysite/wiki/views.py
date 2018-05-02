410f5cbcd7bf50e131d4d0d65ad2bdf04b4c9911
Bradley Steele <bradleysteele2000@gmail.com>
2018-05-02 15:27:48 +0100

    Added index view

--- mysite/wiki/views.py
+++ mysite/wiki/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'wiki/index.html'
+    context_object_name = 'page'
+
+
+def index(request):
+    return render(request, 'wiki/index.html')
