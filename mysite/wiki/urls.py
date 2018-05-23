6759b1c9c7dfbdbc7dcf3637035b5552caa4d8f2
Bradley Steele <bradleysteele2000@gmail.com>
2018-05-23 09:27:52 +0100

    Created logout view

--- mysite/wiki/urls.py
+++ mysite/wiki/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'wiki'
 urlpatterns = [
     path('login/', views.login_view, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
 
     path('', views.index, name='index'),
