6b24627be43128dda06193084d018f312a0b76f2
Bradley Steele <bradleysteele2000@gmail.com>
2018-05-23 13:50:01 +0100

    Registered UploadedFile

--- mysite/wiki/admin.py
+++ mysite/wiki/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from wiki.models import Page
+from wiki.models import Page, UploadedFile
 
 # Registers the Page object with the admin site, so we can
 # handle Page objects using the admin UI.
 admin.site.register(Page)
+admin.site.register(UploadedFile)
