65ab2ef0a6822ce030dd9616cc081f5d076546d1
Bradley Steele <bradleysteele2000@gmail.com>
2018-05-23 10:00:05 +0100

    Documented class

--- mysite/wiki/forms.py
+++ mysite/wiki/forms.py
@@ -3,6 +3,12 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 class UserLoginForm(AuthenticationForm):
+    """
+    The UserLoginForm is a simple implementation of AuthenticationForm
+    used to provide a username, password and any error messages to
+    the login.html template.
+    """
+
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
     error = None
