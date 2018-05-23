c9b3bf56ad7044dede16b79f84ca838ab9284b5e
Bradley Steele <bradleysteele2000@gmail.com>
2018-05-23 14:29:49 +0100

    Fixed bug

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     path('wiki/', include('wiki.urls')),
-
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
