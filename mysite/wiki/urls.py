913d55b62ce09e9a1a6b28c7cd4344d9f3f82146
Bradley Steele <bradleysteele2000@gmail.com>
2018-05-23 13:50:43 +0100

    Created upload path

--- mysite/wiki/urls.py
+++ mysite/wiki/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
+    path('upload/', views.upload_view, name='upload'),
 
     path('', views.index_view, name='index'),
     path('<str:title>/', views.detail_view, name='detail'),
